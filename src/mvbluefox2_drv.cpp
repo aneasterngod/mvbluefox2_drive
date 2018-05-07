14b54da21759ca39afcf654e8ae2e3184e74af96
aneasterngod <38875154+aneasterngod@users.noreply.github.com>
2018-05-07 23:38:03 +0900

    Add files via upload

---
+++ src/mvbluefox2_drv.cpp
@@ -0,0 +1,8 @@
+#include "mvbluefox2_io.h"
+
+
+int main(int argc, char** argv){
+
+
+    return 0;
+}
---
+++ src/mvbluefox2_io.cpp
@@ -0,0 +1,10 @@
+#include "mvbluefox2_io.h"
+
+
+mvbluefox2_io::mvbluefox2_io(){
+
+}
+
+mvbluefox2_io::~mvbluefox2_io(){
+	
+}
