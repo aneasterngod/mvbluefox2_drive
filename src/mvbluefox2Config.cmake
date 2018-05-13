16c205a41b356077f572bcf2b18d595c935566ec
aneasterngod <38875154+aneasterngod@users.noreply.github.com>
2018-05-13 22:55:16 +0900

    Update mvbluefox2Config.cmake

--- src/mvbluefox2Config.cmake
+++ src/mvbluefox2Config.cmake
@@ -1,4 +1,4 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/mvbluefox2-targets.cmake)
-get_filename_component(mvbluefox2_INCLUDE_DIRS "${SELF_DIR}/../../include/mvbluefox2" ABSOLUTE)
+get_filename_component(mvbluefox2_INCLUDE_DIRS "${SELF_DIR}/../../include" ABSOLUTE)
 get_filename_component(mvbluefox2_LIBRARY_DIRS "${SELF_DIR}/../../lib" ABSOLUTE)
