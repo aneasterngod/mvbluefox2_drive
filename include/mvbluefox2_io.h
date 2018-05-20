0b5a9ccf096f008d2b44d4730c136612b9c8a005
aneasterngod <38875154+aneasterngod@users.noreply.github.com>
2018-05-20 20:25:41 +0900

    Update mvbluefox2_io.h

--- include/mvbluefox2_io.h
+++ include/mvbluefox2_io.h
@@ -1,17 +1,4 @@
 #include "mvbluefox2_common.h"
-#ifndef IMAGEDATA
-#define IMAGEDATA
-
-class ImageData{
-public:
-	ImageData();
-	~ImageData();
-public:
-	cv::Mat m_img;
-	int64_t m_timestamp;
-
-};
-#endif
 
 #ifndef MVBLUEFOX2_IO
 #define MVBLUEFOX2_IO
